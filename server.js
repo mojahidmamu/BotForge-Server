95638d072d0c4ca53c0d18bedd5c4271d0cb1607
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-14 12:02:08 +0600

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+const dotenv = require('dotenv');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const nodemailer = require('nodemailer');
 
